d052b3425744789072765e0f802733ba51d0af05
Cliford <cliffordrojas@gmail.com>
2019-05-06 18:40:05 -0400

    Updated some UI

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/android/support/constraint/R.java
@@ -73,20 +73,20 @@ public final class R {
     public static final class id {
         private id() {}
 
-        public static final int bottom = 0x7f070023;
-        public static final int end = 0x7f070048;
-        public static final int gone = 0x7f070050;
-        public static final int invisible = 0x7f07005b;
-        public static final int left = 0x7f07005d;
-        public static final int packed = 0x7f07006f;
-        public static final int parent = 0x7f070070;
-        public static final int percent = 0x7f070073;
-        public static final int right = 0x7f070078;
-        public static final int spread = 0x7f070095;
-        public static final int spread_inside = 0x7f070096;
-        public static final int start = 0x7f07009b;
-        public static final int top = 0x7f0700aa;
-        public static final int wrap = 0x7f0700b1;
+        public static final int bottom = 0x7f070025;
+        public static final int end = 0x7f07004a;
+        public static final int gone = 0x7f070052;
+        public static final int invisible = 0x7f07005d;
+        public static final int left = 0x7f07005f;
+        public static final int packed = 0x7f070071;
+        public static final int parent = 0x7f070072;
+        public static final int percent = 0x7f070075;
+        public static final int right = 0x7f07007a;
+        public static final int spread = 0x7f070097;
+        public static final int spread_inside = 0x7f070098;
+        public static final int start = 0x7f07009d;
+        public static final int top = 0x7f0700ac;
+        public static final int wrap = 0x7f0700b3;
     }
     public static final class styleable {
         private styleable() {}
